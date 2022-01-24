0c5c8135755e26e6b91d34626e3b4133a4af1bc6
Nuttapon Jamnianphon <suntunz@gmail.com>
2022-01-24 16:04:44 +0700

    fix testing coverage code

--- src/components/atoms/Counter/Counter.spec.tsx
+++ src/components/atoms/Counter/Counter.spec.tsx
@@ -3,8 +3,16 @@ import React from 'react'
 import userEvent from '@testing-library/user-event'
 import Counter from './Counter'
 
-const renderCounter = () => {
-  render(<Counter description="Counter first" defaultCount={0} />)
+interface RenderCounterArgs {
+  description?: string,
+  defaultCount?: number
+}
+
+const renderCounter = (args: RenderCounterArgs = {}) => {
+  const { description = 'Counter first', defaultCount = 0 } = args
+
+  render(<Counter description={description} defaultCount={defaultCount} />)
+
   const getText = (text: string | RegExp) => screen.getByText(text)
   const getButton = (options: object = {}) => screen.getByRole('button', options)
 
@@ -15,28 +23,98 @@ const renderCounter = () => {
 }
 
 describe('Counter component', () => {
+  describe('initialize with defaultCount=10 and description="Step Counter"', () => {
+    it('renders defaultCount=10 and description="Step Counter"', () => {
+      const { getText } = renderCounter({ description: 'Step Counter', defaultCount: 10 })
+      expect(getText(/current count: 10/i)).toBeInTheDocument()
+      expect(getText(/step counter/i)).toBeInTheDocument()
+    })
+
+    it('render label as Incrementor', () => {
+      const { getText } = renderCounter({ description: 'Step Counter', defaultCount: 10 })
+      expect(getText(/step counter/i)).toBeInTheDocument()
+    })
+
+    describe('when user input and click actions', () => {
+      it('user typing "20" into label input incrementor', () => {
+        renderCounter({ description: 'Step Counter', defaultCount: 10 })
+        const input = screen.getByLabelText(/incrementor/i)
+        userEvent.type(input, '{selectall}20')
+        expect(input).toHaveValue('20')
+      })
+
+      it('user typing text "anythings" into label input incrementor must be not Nan', () => {
+        renderCounter({ description: 'Step Counter', defaultCount: 10 })
+        const input = screen.getByLabelText(/incrementor/i)
+        userEvent.type(input, '{selectall}anythings')
+        expect(input).not.toBeNaN()
+      })
+
+      it('user typing text "anythings" into label input incrementor must be greater than or equal 0', () => {
+        renderCounter({ description: 'Step Counter', defaultCount: 10 })
+        const input = screen.getByLabelText(/incrementor/i)
+        userEvent.type(input, '{selectall}anythings')
+        const parseIntValue = parseInt((input as HTMLInputElement).value, 10)
+        expect(parseIntValue).toBeGreaterThanOrEqual(0)
+      })
+
+      it('when increment changes to 5 and "+" button is clicked', () => {
+        const { getText, getButton } = renderCounter(
+          { description: 'Step Counter', defaultCount: 10 }
+        )
+
+        userEvent.type(screen.getByLabelText(/incrementor/i), '{selectall}5')
+        userEvent.click(getButton({ name: 'Increment' }))
+
+        expect(getText(/current count: 15/i)).toBeInTheDocument()
+      })
+
+      it('when decrement changes to 5 and "-" button is clicked', () => {
+        const { getText, getButton } = renderCounter(
+          { description: 'Step Counter', defaultCount: 10 }
+        )
+
+        userEvent.type(screen.getByLabelText(/incrementor/i), '{selectall}5')
+        userEvent.click(getButton({ name: 'Decrement' }))
+
+        expect(getText(/current count: 5/i)).toBeInTheDocument()
+      })
+
+      it('when decrement changes the minimun number must be 0', () => {
+        const { getText, getButton } = renderCounter(
+          { description: 'Step Counter', defaultCount: 10 }
+        )
+
+        userEvent.type(screen.getByLabelText(/incrementor/i), '{selectall}15')
+        userEvent.click(getButton({ name: 'Decrement' }))
+
+        expect(getText(/current count: 0/i)).toBeInTheDocument()
+      })
+    })
+  })
+
   describe('initialize with defaultCount=0 and description="Counter first"', () => {
-    it('renders default count = 0', () => {
+    it('renders defaultCount=0', () => {
       const { getText } = renderCounter()
       expect(getText(/current count: 0/i)).toBeInTheDocument()
     })
 
-    it('renders title as Counter first', () => {
+    it('renders description as Counter first', () => {
       const { getText } = renderCounter()
       expect(getText(/counter first/i)).toBeInTheDocument()
     })
 
     describe('when user click actions', () => {
-      it('defaultCount = 0, and + clicked then counter = 1', () => {
+      it('user checked + then current count = 1', () => {
         const { getText, getButton } = renderCounter()
-        userEvent.click(getButton({ name: '+' }))
+        userEvent.click(getButton({ name: 'Increment' }))
         expect(getText(/current count: 1/i)).toBeInTheDocument()
       })
 
-      it('defaultCount = 0, and - clicked then couter = -1', () => {
+      it('user clicked - then current count = 0', () => {
         const { getText, getButton } = renderCounter()
-        userEvent.click(getButton({ name: '-' }))
-        expect(getText(/current count: -1/i)).toBeInTheDocument()
+        userEvent.click(getButton({ name: /Decrement/i }))
+        expect(getText(/current count: 0/i)).toBeInTheDocument()
       })
     })
   })
--- src/components/atoms/Counter/Counter.tsx
+++ src/components/atoms/Counter/Counter.tsx
@@ -6,14 +6,37 @@ interface CounterProps {
 }
 
 const Counter = ({ description, defaultCount }: CounterProps) => {
-  const [count, setCount] = useState(defaultCount)
+  const [count, setCount] = useState<number>(defaultCount)
+  const [incrementor, setIncrementor] = useState<number>(10)
+
+  const handlerSetCount = () => {
+    const newStateCount = count - incrementor
+    setCount(newStateCount < 0 ? 0 : newStateCount)
+  }
+
+  const handlerSetIncrementor = (value: string) => {
+    const parseIntValue = parseInt(value, 10)
+    setIncrementor(parseIntValue || 0)
+  }
 
   return (
     <div>
       <h2>Desc: {description} - Current Count: {count}</h2>
 
-      <button type="button" onClick={() => setCount(count - 1)}> - </button>
-      <button type="button" onClick={() => setCount(count + 1)}> + </button>
+      <div>
+        <label htmlFor="input-incrementor">
+          Incrementor
+          <input
+            id="input-incrementor"
+            type="text"
+            value={incrementor}
+            onChange={(event) => handlerSetIncrementor(event.target.value)}
+          />
+        </label>
+      </div>
+
+      <button type="button" aria-label="Decrement" onClick={() => handlerSetCount()}> - </button>
+      <button type="button" aria-label="Increment" onClick={() => setCount(count + incrementor)}> + </button>
     </div>
   )
 }
